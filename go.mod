ae0fe47b7a12d91ec87d2d5e7e6d3650c83d57ae
Vladimir Ermakov <vooon341@gmail.com>
2026-02-11 12:35:05 +0100

    update server to v5

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/vooon/esp-ota-server
 
-go 1.25.4
+go 1.25.7
 
 require (
 	github.com/alecthomas/kong v1.14.0
--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"crypto/sha512"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -13,7 +14,6 @@ import (
 	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
-	"github.com/labstack/gommon/log"
 
 	"github.com/vooon/esp-ota-server/assets"
 )
@@ -24,13 +24,7 @@ type server struct {
 }
 
 // Render renders a template document
-func (s server) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-
-	// Add global methods if data is a map
-	if viewContext, isMap := data.(map[string]interface{}); isMap {
-		viewContext["reverse"] = c.Echo().Reverse
-	}
-
+func (s server) Render(_ *echo.Context, w io.Writer, name string, data any) error {
 	return s.templates.ExecuteTemplate(w, name, data)
 }
 
@@ -42,7 +36,7 @@ func getEspHeader(hdr http.Header, key string) (ret []string, ok bool) {
 	return
 }
 
-func (s server) getBinaryFile(c echo.Context) error {
+func (s server) getBinaryFile(c *echo.Context) error {
 	lg := c.Logger()
 
 	project := c.Param("project")
@@ -51,11 +45,7 @@ func (s server) getBinaryFile(c echo.Context) error {
 	path := filepath.Join(s.config.DataDirPath, project, filename)
 	file, err := os.Open(path)
 	if err != nil && os.IsNotExist(err) {
-		lg.Warnj(log.JSON{
-			"msg":       "File not found",
-			"err":       err,
-			"file_path": path,
-		})
+		lg.Warn("File not found", "err", err, "file_path", path)
 		return c.String(http.StatusNotFound, "no file")
 	} else if err != nil {
 		return err
@@ -77,9 +67,7 @@ func (s server) getBinaryFile(c echo.Context) error {
 
 	hdr := c.Request().Header
 
-	lg.Printj(log.JSON{
-		"esp_request_headers": hdr,
-	})
+	lg.Info("ESP request headers", "headers", hdr)
 
 	//staMac, _ := hdr["X-Esp8266-Sta-Mac"]
 	//apMac, _ := hdr["X-Esp8266-Ap-Mac"]
@@ -102,9 +90,7 @@ func (s server) getBinaryFile(c echo.Context) error {
 			return err
 		}
 
-		c.Logger().Printj(log.JSON{
-			"esp_version_map": vmap,
-		})
+		c.Logger().Info("ESP version map", "version_map", vmap)
 
 		// if version has MD5
 		md5, mok := vmap["md5"]
@@ -118,12 +104,7 @@ func (s server) getBinaryFile(c echo.Context) error {
 
 	c.Response().Header()["x-MD5"] = []string{md5sum} // do not do strings.Title()
 	c.Response().Header().Set("x-SHA512", sha512sum)  // not used by actual version
-	lg.Printj(log.JSON{
-		"esp_mode":  mode[0],
-		"send_file": sendFile,
-		"file_path": path,
-		"file_size": len(b),
-	})
+	lg.Info("OTA decision", "esp_mode", mode[0], "send_file", sendFile, "file_path", path, "file_size", len(b))
 
 	if sendFile {
 		//return c.Blob(http.StatusOK, "application/ocetet-stream", b)
@@ -133,8 +114,8 @@ func (s server) getBinaryFile(c echo.Context) error {
 	}
 }
 
-func (s server) get403(c echo.Context) error {
-	return c.Render(http.StatusForbidden, "403.ghtm", map[string]interface{}{
+func (s server) get403(c *echo.Context) error {
+	return c.Render(http.StatusForbidden, "403.ghtm", map[string]any{
 		"BarbradyJpgBase64": "/assets/barbrady.jpg",
 	})
 }
@@ -146,7 +127,7 @@ func parseTemplates() (*template.Template, error) {
 func Serve(config Config) error {
 	e := echo.New()
 
-	e.Use(middleware.Logger())
+	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
 	if config.EnablePrometheus {
 		e.Use(echoprometheus.NewMiddleware("espota"))
@@ -154,16 +135,17 @@ func Serve(config Config) error {
 
 	newpath, err := filepath.Abs(config.DataDirPath)
 	if err != nil {
-		e.Logger.Fatal("can't abs data-dir")
-		return err
+		return fmt.Errorf("can't abs data-dir: %w", err)
 	}
-	if stat, err := os.Stat(newpath); err == nil && stat.IsDir() {
-		e.Logger.Info("Data-dir: ", newpath)
-		config.DataDirPath = newpath
-	} else {
-		e.Logger.Fatal("data-dir not exist! ", newpath)
-		return err
+	stat, err := os.Stat(newpath)
+	if err != nil {
+		return fmt.Errorf("data-dir stat failed for %q: %w", newpath, err)
 	}
+	if !stat.IsDir() {
+		return fmt.Errorf("data-dir is not a directory: %s", newpath)
+	}
+	e.Logger.Info("Data-dir", "path", newpath)
+	config.DataDirPath = newpath
 
 	templates, err := parseTemplates()
 	if err != nil {
